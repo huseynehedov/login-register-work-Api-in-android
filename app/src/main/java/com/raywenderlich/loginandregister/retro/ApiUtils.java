c5f978d1c4dd4c63398dbbfbc728962b6b1f5e67
huseynehedov <79727323+huseynehedov@users.noreply.github.com>
2022-04-08 02:11:27 +0400

    Update ApiUtils.java

--- app/src/main/java/com/raywenderlich/loginandregister/retro/ApiUtils.java
+++ app/src/main/java/com/raywenderlich/loginandregister/retro/ApiUtils.java
@@ -5,7 +5,7 @@ import com.raywenderlich.loginandregister.dao.UsersDaoInterface;
 
 public class ApiUtils {
 
-    public static final String baseUrl = "http://94.20.38.107:8003/api/";
+    public static final String baseUrl = "";
 
     public static UsersDaoInterface getUsersDaoInterface(){
         return RetrofitClient.getClient(baseUrl).create(UsersDaoInterface.class);
